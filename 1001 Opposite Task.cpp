23189230c7ad33e75134821d0a95bb9011854ec1
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-05-31 00:04:39 +0600

    Create 1001 Opposite Task.cpp

---
+++ 1001 Opposite Task.cpp
@@ -0,0 +1,20 @@
+//    https://lightoj.com/problem/opposite-task
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t, n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		if(n>10)
+		{
+			cout<<"10 "<<n-10<<endl;
+		}
+		else cout<<0<<" "<<n<<endl;
+	}
+}
