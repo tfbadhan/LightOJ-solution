72075a38f629cbc24a9af17eda060c4d8b3f649d
Toushiful Ferdous Badhan <68116912+tfbadhan@users.noreply.github.com>
2021-05-31 11:09:16 +0600

    Create 1022 Circle in Square.cpp

---
+++ 1022 Circle in Square.cpp
@@ -0,0 +1,25 @@
+//    https://lightoj.com/problem/circle-in-square
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int t;
+    double pi = 2.0 * acos(0.0);
+	
+	scanf("%d", &t);
+	
+	for(int i=1; i<=t; i++)
+	{
+		double r;
+		scanf("%lf", &r);
+		
+		double res=(2*r * 2*r)- (pi*r*r);
+		
+		printf("Case %d: %.2lf\n", i, res);
+	}
+}
